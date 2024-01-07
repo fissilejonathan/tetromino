9a7ff5ef7a625d987f9444d862c3609d500a8405
jmoralesss <mail@jmorales.net>
2024-01-07 16:32:27 -0600

    added basic input controls

--- cmd/root.go
+++ cmd/root.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"os"
 
+	"github.com/eiannone/keyboard"
 	"github.com/spf13/cobra"
 )
 
@@ -11,7 +12,47 @@ var rootCmd = &cobra.Command{
 	Use:   "tetro",
 	Short: "cli tetromino",
 	Run: func(cmd *cobra.Command, args []string) {
-		// Do Stuff Here
+		input := make(chan keyboard.Key)
+		done := make(chan struct{})
+
+		err := keyboard.Open()
+		if err != nil {
+			panic(err)
+		}
+		defer func() {
+			keyboard.Close()
+		}()
+
+		// handle user input
+		go func() {
+			defer close(input)
+			for {
+				_, key, err := keyboard.GetSingleKey()
+				if err != nil {
+					fmt.Println("Error reading input:", err)
+					return
+				}
+
+				if key == keyboard.KeyEsc {
+					close(done)
+				}
+
+				if key == keyboard.KeyArrowDown || key == keyboard.KeyArrowUp || key == keyboard.KeyArrowLeft || key == keyboard.KeyArrowRight {
+					input <- key
+				}
+			}
+		}()
+
+		for {
+			select {
+			case userInput := <-input:
+				fmt.Printf("Received: %c\n", userInput)
+			case <-done:
+				return
+			default:
+				// Do other non-blocking tasks here
+			}
+		}
 	},
 }
 
--- go.mod
+++ go.mod
@@ -5,6 +5,8 @@ go 1.20
 require github.com/spf13/cobra v1.8.0
 
 require (
+	github.com/eiannone/keyboard v0.0.0-20220611211555-0d226195f203 // indirect
 	github.com/inconshreveable/mousetrap v1.1.0 // indirect
 	github.com/spf13/pflag v1.0.5 // indirect
+	golang.org/x/sys v0.16.0 // indirect
 )
